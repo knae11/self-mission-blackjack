86f9c0beb96218f9e3fa4e141c52eb713bf950cd
naeunkim <knae11@naver.com>
2021-06-11 17:32:59 +0900

    feat: createRoom Service 구현

--- src/main/java/blackjack/application/BlackjackService.java
+++ src/main/java/blackjack/application/BlackjackService.java
@@ -1,10 +1,24 @@
 package blackjack.application;
 
+import blackjack.dao.RoomDao;
+import blackjack.domain.card.Deck;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlackjackService {
+
+    private final RoomDao roomDao;
+
+    public BlackjackService(RoomDao roomDao) {
+        this.roomDao = roomDao;
+    }
+
     public Long createRoom() {
-        return 1L;
+        Deck deck = Deck.createShuffled();
+        List<String> cardIds = deck.getCardIds();
+        String cardDeck = String.join("/", cardIds);
+        return roomDao.createRoom(cardDeck);
     }
 }
